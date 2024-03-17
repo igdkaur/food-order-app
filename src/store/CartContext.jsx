72a92918d0a0141a3ff79474ea173a50b7001cab
GDK <igdkaur@gmail.com>
2024-03-17 13:17:11 +0530

    wip context

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,13 +1,63 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from "react";
 
-const CartContext = createContext({ // spread
+const CartContext = createContext({
   items: [],
-  additem: item => {},
-  removeItem: id => {}
-})
+  additem: (item) => {},
+  removeItem: (id) => {},
+});
 
- export function CartContextProvider({children}) { // manage
-  return <CartContextProvider></CartContextProvider>
+function cartReducer(state, action) {
+  if (action.type === 'ADD_ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+
+   if (action.type === 'REMOVE_ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+
+
+export function CartContextProvider({ children }) {
+  useReducer(cartReducer, { items: []});
+  return <CartContextProvider></CartContextProvider>;
 }
 
 export default CartContext;
