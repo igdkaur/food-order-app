37975070d6c9fff937570a2fcb5660d363ae718b
GDK <igdkaur@gmail.com>
2024-03-18 09:02:53 +0530

    cart quantity update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from "../components/Button";
+import CartContext from '../store/CartContext';
 
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +18,7 @@ export default function Header() {
         <h1>TikkaTickle</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,19 +1,31 @@
-import Button from "../components/Button";
+import { useContext } from 'react';
+
 import { currencyFormatter } from '../util/formatting.js';
+import Button from '../components/Button';
+import CartContext from '../store/CartContext.jsx';
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    console.log("heyuuuuuuuuuuuuuuuuuuu");
+    cartCtx.addItem(meal);
+    
+  }
+
   return (
     <li className="meal-item">
       <article>
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{currencyFormatter.format(meal.price)}
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
           </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button textOnly >Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
