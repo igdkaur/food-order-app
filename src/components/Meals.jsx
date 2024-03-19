1db8ff65967a332e63ab133896013ee3eae1bc84
GDK <igdkaur@gmail.com>
2024-03-19 08:37:55 +0530

    wip custom hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,24 +1,29 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-
-  useEffect(()=> {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", {}, []);
   
-      if (!response.ok) {
-        //...
-      }
-  
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
+  // const [loadedMeals, setLoadedMeals] = useState([]);
 
-  }, []);
+  // useEffect(()=> {
+  //   async function fetchMeals() {
+  //     const response = await fetch('http://localhost:3000/meals');
 
+  //     if (!response.ok) {
+  //       //...
+  //     }
+
+  //     const meals = await response.json();
+  //     setLoadedMeals(meals);
+  //   }
+  //   fetchMeals();
+
+  // }, []);
 
   return (
     <ul id="meals">
